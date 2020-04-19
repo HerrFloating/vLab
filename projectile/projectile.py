9e298ec0e0a4555561fa7cb46340f4872e7d234e
Mahan <satarpour.mahan@gmail.com>
2020-04-19 23:42:49 +0430

    Ground fixed

--- projectile/projectile.py
+++ projectile/projectile.py
@@ -13,22 +13,27 @@ theta = 30
 scene1 = canvas(center = vector(8,5,0),background = color.white)
 """Object"""
 mass = sphere(pos = vector(0,10,0),velocity = vector(v_0*cos(radians(theta)),v_0*sin(radians(theta)),0),radius = 0.5,color = color.blue , make_trail = False)
+"""ground"""
+ground = cylinder(pos = vector(mass.pos.x,0,0),axis = vector(5,0,0),radius = 0.1,color = color.black)
 """Run button"""
 running = False
 def Run(b):
     global mass,running
     running = not running
-    if running: b.text = "Pause"
-    else: b.text = "Run"
+    if running: 
+        b.text = "Pause"
+    else: 
+        b.text = "Run"
 button(text = 'Run' , bind = Run)
 """Reset button"""
 def Reset(c):
-    global  W1,t , mass , spring
+    global  W1,t , mass , spring, ground
     t = 0
     mass.pos = vector(0,10,0)
     mass.velocity = vector(v_0*cos(radians(theta)),v_0*sin(radians(theta)),0)
     mass.clear_trail()
     W1.text = 'Elapsed time : 0'
+    ground.pos.x = mass.pos.x
 button(text = 'Reset' , bind = Reset)
 scene1.append_to_caption('\n\n')
 """alpha input"""
@@ -70,9 +75,10 @@ def up():
     mass.make_trail = False
     drag = False
 while True:
+    ground.axis.x = mass.pos.x
     scene1.bind("mousedown", down)
 
-    scene1.bind("mousemove", move)
+    scene1.bind("mousemove", move) 
 
     scene1.bind("mouseup", up)
     W2.text = "x Component : ",scene1.mouse.pos.x
@@ -93,7 +99,7 @@ while True:
             alpha_in.disabled = True
             beta_in.disabled = True
             v.disabled = True
-            theta1.disabled = True
+            theta1.disabled = True    
             phi = atan(mass.velocity.y/mass.velocity.x)
             mass.make_trail = True
             mass.pos = mass.velocity*dt + mass.pos
